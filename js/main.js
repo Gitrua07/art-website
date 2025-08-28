4ecc21aa9a5f5dc5855becd2264821e345fa0453
Gaia Iturralde <138412962+Gitrua07@users.noreply.github.com>
2025-08-28 14:54:12 -0700

    Created lightbox for enlarging image. Used event listener to display and remove enlarged image

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+const gallery = document.getElementById('gallery');
+
 fetch('list/index.json')
     .then(res => res.json())
     .then(files => {
@@ -5,14 +7,31 @@ fetch('list/index.json')
             fetch(`${file}`)
                 .then(res => res.json())
                 .then(data => {
-                    const gallery = document.getElementById('gallery');
                     const image = document.createElement('div');
                     image.className = 'artwork';
+
                     const img = document.createElement('img');
                     img.src = data.image;
                     img.alt = data.description;
+
                     image.append(img);
                     gallery.append(image);
+
+                    img.addEventListener('click', () => {
+                        lightbox.style.display = 'flex';
+                        lightbox_img.src = img.src;
+                        lightbox_img.alt = img.alt;
+                    })
                 });
         });
     });
+
+const artworks = document.querySelectorAll('.artwork img');
+
+const lightbox = document.getElementById('lightbox');
+const lightbox_img = document.getElementById('lightbox-img');
+const close_btn = document.getElementById('close');
+
+close_btn.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+})
