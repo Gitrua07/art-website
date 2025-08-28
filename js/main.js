694295f757695cb8b9fb25540ae8cd549cd9c8a1
Gaia Iturralde <138412962+Gitrua07@users.noreply.github.com>
2025-08-27 20:19:50 -0700

    Implemented images

---
+++ js/main.js
@@ -0,0 +1,18 @@
+fetch('artworks/index.json')
+.then(res => res.json())
+.then(files => {
+    files.forEach(file =>{
+        fetch(`${file}`)
+        .then(res => res.json())
+        .then(data => {
+            const gallery = document.getElementById('gallery');
+            const image = document.createElement('div');
+            image.className = 'artwork';
+            const img = document.createElement('img');
+            img.src = data.image;
+            img.alt = data.description;
+            image.append(img);
+            gallery.append(image);
+        });
+    });
+});
