9d6c190e8f36f7d6e96e31306c059b3197061b18
Gaia Iturralde <Iturralde.gaia@gmail.com>
2026-07-11 01:37:41 -0700

    Implemented click image -> zoom in solo feature

--- src/db.js
+++ src/db.js
@@ -1,31 +1,51 @@
 const paintings = [
     {
         id: 1,
-        title: "All-Star Air-On Gordon",
-        year: 2025,
-        material: 'Digital',
+        title: "Exercise on Colour Theory",
+        year: 2026,
+        material: 'Acrylic',
         size: '2000x2000',
-        img: '/img/dunk_contest.jpg',
+        img: '/img/a1/cool-colours.JPEG',
         alt: 'Aaron Gordon dunking at the all-star game \nIllustrated with Procreate',
-    },
-    {
-        id: 2,
-        title: 'Blonde woman',
-        year: 2023,
-        material: 'Digital',
-        size: '2000x2000',
-        img:'/img/logo.JPG',
-        alt: 'Girl looking up at camera on a clear blue day.',
-    },
-    {
-        id: 3,
-        title: 'Band of Two',
-        year: 2025,
-        material: 'Digital',
-        size: '2000x2000',
-        img: '/img/band-members.jpg',
-        alt: 'A duo performs a song on a stage\nIllustrated with Procreate',
-    },
+        images: [
+            {
+                src:'/img/a1/brush-explorations.JPEG',
+                alt: 'brush exploration exercise'
+            },
+            {
+                src: '/img/a1/b-w-value-scale.JPEG',
+                alt: 'b-w value scale exercise'
+            },
+            {
+                src: '/img/a1/small-white-object.JPEG',
+                alt: 'small white object exercise'
+            },
+            {
+                src: '/img/a1/colour-value-scale.JPEG',
+                alt: 'colour value scale exercise'
+            },
+            {
+                src: '/img/a1/value-into-colour.JPEG',
+                alt: 'value into colour exercise'
+            },
+            {
+                src: '/img/a1/chroma-swatches.JPEG',
+                alt: 'chroma swatches exercise'
+            },
+            {
+                src: '/img/a1/warm-colours.JPEG',
+                alt: 'warm colours exercise'
+            },
+            {
+                src: '/img/a1/cool-colours.JPEG',
+                alt: 'cool colours exercise'
+            },
+            {
+                src: '/img/a1/analogous-palette.JPG',
+                alt: 'analogous palette exercise'
+            }            
+        ]
+    }
 ]
 
 export default paintings
--- src/pages/Painting.jsx
+++ src/pages/Painting.jsx
@@ -1,21 +1,30 @@
 import { useParams } from 'react-router-dom'
 import paintings from '../db'
+import { useState } from 'react'
 
 const Painting = () => {
     const id = useParams()
-    const { _, title, year, material, size, img, alt } = paintings.find(val => val.id == id.id)
+    const { title, year, material,images} = paintings.find(val => val.id == id.id)
+    const [selectedImage, setSelectedImage] = useState()
 
     return (
         <div className='paintings-info'>
             <div className='full-image'>
-                <img className='painting-image' src={img} alt={alt} />
+                {images.map(image => {
+                    return(<img key={image.alt} onClick={() => setSelectedImage(image)} className='painting-image' src={image.src} alt={image.alt} />)
+                })}
             </div>
             <div className='full-text'>
                 <h2>Assignment {id.id}: {title} </h2>
                 <div><b>Year:</b> {year}</div>
                 <div><b>Material:</b> {material}</div>
-                <div><b>Size:</b> {size}</div>
             </div>
+
+            {selectedImage && (
+                <div onClick={() => setSelectedImage()} className='popup-tab'>
+                    <img src={selectedImage.src} alt={selectedImage.alt}/>
+                </div>
+            )}
         </div>
     )
 }
