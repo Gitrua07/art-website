df50ebd6553d15a23d709dbb8c772d0af82cb8a8
Gaia Iturralde <Iturralde.gaia@gmail.com>
2026-07-24 21:24:15 -0700

    Implemented assignment 2

--- src/db.js
+++ src/db.js
@@ -9,7 +9,7 @@ const paintings = [
         alt: 'Shows the personal palette exercise for preview to A1',
         images: [
             {
-                src:'/img/a1/brush-exploration.JPEG',
+                src: '/img/a1/brush-exploration.JPEG',
                 alt: 'brush exploration exercise'
             },
             {
@@ -51,9 +51,71 @@ const paintings = [
             {
                 src: '/img/a1/personal-palette.JPEG',
                 alt: 'personal palette exercise'
-            }            
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "Autobiographic Still-Life",
+        date: "July 25, 2026",
+        material: 'Acrylic',
+        size: '',
+        img: '/img/a2/still-study.JPEG',
+        alt: 'Shows the Autobiographic Still-Life paintings for preview to A2',
+        rationale: `My 12" x 16" still-life painting shows a pencil case fox, a UVIC cap, lip gloss, and a silver ring around the pencil case's ear. The items lay upon an orange blanket, with curtains in the background. I chose each item because it currently has significance in my life. I bring my pencil case fox with me regularly to school. It represents the academic part which is important because without the tools that the pencil case contains I won't be able to perform to my best ability. I wear my UVIC hat with me everywhere. It represents my casual life as a student; this represents my time hanging out with friends and participating in school events. The lip gloss and silver ring is something I wear occasionally when I want to feel confident and put together. This represents my desire to express my creativity and personality in my art because when I feel confident that is when I most want to create.
+
+While painting this still-life, I learned about the use of saturation to differentiate the background distance. Moreover, I learned that you can apply the point of interest principle in still lifes. I found the effects of contrast and value to a painting interesting. It is evident that value placement is one of the most important things to apply purposefully when creating paintings.
+
+During this process, I found the following challenging:
+- Composition: I found finding the right composition challenging because I had to think about the overall concept of my painting.
+- Painting different distances: I found that differentiating distance through hues was challenging because it is hard to mix paints to different saturations.
+- Colour mixing: I found that mixing colours to get the right/accurate hue was difficult. This is amplified by my goal to save as much paint as I can which was also another difficulty; I ended up with more paint than I needed after I finished my painting.
+
+Furthermore, I found the following successes:
+- Applying refined detail: I used smaller and more pointed brushes to apply more refined details.
+- Painting texture: I used different stroke techniques to recreate textures like my hat, and used highly contrasted values to recreate metal for the ring.
+- Value contrast: I applied a noticeable value contrast depending on if the item is in the foreground, middleground, or background. What I did was keep reapplying more extreme values (lighter/darker) on the painting per layer.
+
+When taking into account all the successes and challenges, next time, I would change the composition so that the items overlap more, making the background, middleground and foreground clearer.`,
+        images: [
+            {
+                title: 'Assignment Exercise Work-In-Progress 1',
+                src: '/img/a2/exercises0.JPEG',
+                alt: 'value exercise step 1'
+            },
+            {
+                title: 'Assignment Exercise Work-In-Progress 2',
+                src: '/img/a2/exercise1.JPEG',
+                alt: 'value exercise step 2'
+            },
+            {
+                title: 'Assignment Exercise Final',
+                src: '/img/a2/exercise-final.JPEG',
+                alt: 'value exercise final result'
+            },
+            {
+                title: 'Still-Study Sketches and Value Study',
+                src: '/img/a2/sketch-shadow-study.JPEG',
+                alt: 'sketches and value study'
+            },
+            {
+                title: 'Autobiographic Still-Life Work-In-Progress 1',
+                src: '/img/a2/still-study-wip1.JPEG',
+                alt: 'Autobiographic Still-Life part 1'
+            },
+            {
+                title: 'Autobiographic Still-Life Work-In-Progress 2',
+                src: '/img/a2/still-study-wip2.JPEG',
+                alt: 'Autobiographic Still-Life part 2'
+            },
+            {
+                title: 'Autobiographic Still-Life Final',
+                src: '/img/a2/still-study.JPEG',
+                alt: 'Autobiographic Still-Life final art'
+            }
         ]
     }
+
 ]
 
 export default paintings
--- src/pages/Painting.jsx
+++ src/pages/Painting.jsx
@@ -4,25 +4,33 @@ import { useState } from 'react'
 
 const Painting = () => {
     const id = useParams()
-    const { title, date, material,images} = paintings.find(val => val.id == id.id)
+    const { title, date, material, images, rationale } = paintings.find(val => val.id == id.id)
     const [selectedImage, setSelectedImage] = useState()
 
     return (
         <div className='paintings-info'>
             <div className='full-image'>
                 {images.map(image => {
-                    return(<img key={image.alt} onClick={() => setSelectedImage(image)} className='painting-image' src={image.src} alt={image.alt} />)
+                    return (
+                        <div className='painting-image'>
+                            <img key={image.alt} onClick={() => setSelectedImage(image)} src={image.src} alt={image.alt} />
+                            {image.title && (<div className='image-title'>{image.title}</div>)}
+                        </div>
+                    )
                 })}
             </div>
             <div className='full-text'>
                 <h2>Assignment {id.id}: {title} </h2>
                 <div><b>Date:</b> {date}</div>
                 <div><b>Material:</b> {material}</div>
+                {rationale && (
+                    <div><div><b>Rationale</b>:</div><div className='rationale-text'>{rationale}</div></div>
+                )}
             </div>
 
             {selectedImage && (
                 <div onClick={() => setSelectedImage()} className='popup-tab'>
-                    <img src={selectedImage.src} alt={selectedImage.alt}/>
+                    <img src={selectedImage.src} alt={selectedImage.alt} />
                 </div>
             )}
         </div>
